55e925db2154260fdba34daa62de5ee1a4501cf7
nalbam <me@nalbam.com>
2018-05-19 11:21:37 +0900

    make

--- make.sh
+++ make.sh
@@ -6,5 +6,5 @@ V=$1
 
 echo "" > /tmp/make-${V}.log
 echo ">> start ${V}.. $(date)" >> /tmp/make-${V}.log
-make ${V}                   >> /tmp/make-${V}.log
-echo ">> end ${V}..   $(date)"   >> /tmp/make-${V}.log
+make ${V}                      >> /tmp/make-${V}.log
+echo ">> end ${V}..   $(date)" >> /tmp/make-${V}.log
