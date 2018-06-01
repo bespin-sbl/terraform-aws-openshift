2b7a1778fb0b71c895e2cfdf0b9f159eba420322
nalbam <me@nalbam.com>
2018-06-01 17:21:21 +0900

    eip

--- modules/openshift/99-outputs.tf
+++ modules/openshift/99-outputs.tf
@@ -20,7 +20,7 @@ output "node1-public_dns" {
   value = "${aws_instance.node1.public_dns}"
 }
 output "node1-public_ip" {
-  value = "${length(var.node_eip) < 2 ? element(aws_eip.node1.*.public_ip, 0) : element(var.node_eip, 0)}"
+  value = "${length(var.node_eip) < 2 ? element(aws_eip.node1.*.public_ip, 0) : element(data.aws_eip.node1.*.public_ip, 0)}"
 }
 output "node1-private_dns" {
   value = "${aws_instance.node1.private_dns}"
@@ -33,7 +33,7 @@ output "node2-public_dns" {
   value = "${aws_instance.node2.public_dns}"
 }
 output "node2-public_ip" {
-  value = "${length(var.node_eip) < 2 ? element(aws_eip.node2.*.public_ip, 0) : element(var.node_eip, 1)}"
+  value = "${length(var.node_eip) < 2 ? element(aws_eip.node2.*.public_ip, 0) : element(data.aws_eip.node2.*.public_ip, 0)}"
 }
 output "node2-private_dns" {
   value = "${aws_instance.node2.private_dns}"
