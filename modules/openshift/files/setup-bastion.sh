fc9994404a809f402d76807a7fa52012f71690f8
nalbam <me@nalbam.com>
2018-06-04 16:03:15 +0900

    set-x

--- modules/openshift/files/setup-bastion.sh
+++ modules/openshift/files/setup-bastion.sh
@@ -3,5 +3,9 @@
 # This script template is expected to be populated during the setup of a
 # OpenShift bastion. It runs on host startup.
 
+# Log everything we do.
+set -x
+exec > /var/log/user-data.log 2>&1
+
 curl -s http://repo.toast.sh/helper/slack.sh | \
  bash -s -- --token=TATRUQ6P2/BAY9WSD7C/1bCckidSMB8KctWf2CgbHGtN --color=good Started: \`bastion\` $(hostname)
