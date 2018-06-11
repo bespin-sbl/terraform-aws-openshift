ab1d9a30f381d08503c0945ff49858753372b08a
nalbam <me@nalbam.com>
2018-06-11 19:13:11 +0900

    alb-ssl

--- modules/openshift/07-dns.tf
+++ modules/openshift/07-dns.tf
@@ -3,9 +3,8 @@
 
 //  Create the private DNS.
 resource "aws_route53_zone" "private" {
-  name = "${var.cluster_name}.local"
-  comment = "OpenShift Cluster Private DNS"
-  vpc_id = "${data.aws_vpc.openshift.id}"
+  name    = "${var.cluster_name}.local"
+  vpc_id  = "${data.aws_vpc.openshift.id}"
   tags {
     Name    = "${var.cluster_name} Private DNS"
     Project = "openshift"
@@ -15,27 +14,27 @@ resource "aws_route53_zone" "private" {
 //  Routes for 'master', 'node1' and 'node2'.
 resource "aws_route53_record" "master-record" {
     zone_id = "${aws_route53_zone.private.zone_id}"
-    name = "master.${aws_route53_zone.private.name}"
-    type = "A"
-    ttl  = 300
+    name    = "master.${aws_route53_zone.private.name}"
+    type    = "A"
+    ttl     = 300
     records = [
         "${aws_instance.master.private_ip}"
     ]
 }
 resource "aws_route53_record" "node1-record" {
     zone_id = "${aws_route53_zone.private.zone_id}"
-    name = "node1.${aws_route53_zone.private.name}"
-    type = "A"
-    ttl  = 300
+    name    = "node1.${aws_route53_zone.private.name}"
+    type    = "A"
+    ttl     = 300
     records = [
         "${aws_instance.node1.private_ip}"
     ]
 }
 resource "aws_route53_record" "node2-record" {
     zone_id = "${aws_route53_zone.private.zone_id}"
-    name = "node2.${aws_route53_zone.private.name}"
-    type = "A"
-    ttl  = 300
+    name    = "node2.${aws_route53_zone.private.name}"
+    type    = "A"
+    ttl     = 300
     records = [
         "${aws_instance.node2.private_ip}"
     ]
@@ -44,25 +43,76 @@ resource "aws_route53_record" "node2-record" {
 //  Create the public DNS.
 data "aws_route53_zone" "public" {
   count = "${var.base_domain != "" ? 1 : 0}"
-  name = "${var.base_domain}"
+  name  = "${var.base_domain}"
+}
+data "aws_acm_certificate" "public" {
+  count    = "${var.base_domain != "" ? 1 : 0}"
+  domain   = "${var.base_domain}"
+  statuses = [
+    "ISSUED"
+  ]
+  most_recent = true
+}
+
+resource "aws_lb" "console" {
+  count           = "${var.base_domain != "" ? 1 : 0}"
+  name            = "${var.cluster_name}-console-lb"
+  subnets         = ["${aws_subnet.public.*.id}"]
+  security_groups = [
+    "${aws_security_group.openshift-vpc.id}",
+    "${aws_security_group.openshift-public-ingress.id}",
+    "${aws_security_group.openshift-public-egress.id}",
+  ]
+  tags {
+    Name = "${var.cluster_name} Console LB"
+  }
+}
+resource "aws_lb_target_group" "console" {
+  count    = "${var.base_domain != "" ? 1 : 0}"
+  name     = "${var.cluster_name}-console-tg"
+  port     = "8443"
+  protocol = "HTTPS"
+  vpc_id   = "${data.aws_vpc.openshift.id}"
+  tags {
+    Name = "${var.cluster_name} Console LB TG"
+  }
+}
+resource "aws_lb_target_group_attachment" "console" {
+  count            = "${var.base_domain != "" ? 1 : 0}"
+  target_group_arn = "${aws_lb_target_group.console.arn}"
+  target_id        = "${aws_instance.master.id}"
+  port             = "8443"
+}
+resource "aws_lb_listener" "console_http" {
+  count             = "${var.base_domain != "" ? 1 : 0}"
+  load_balancer_arn = "${aws_lb.console.arn}"
+  port              = "8443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${data.aws_acm_certificate.public.arn}"
+  default_action {
+    target_group_arn = "${aws_lb_target_group.console.arn}"
+    type             = "forward"
+  }
 }
 
 resource "aws_route53_record" "console" {
-  count = "${var.base_domain != "" ? 1 : 0}"
+  count   = "${var.base_domain != "" ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.public.zone_id}"
-  name = "console.${data.aws_route53_zone.public.name}"
-  type = "A"
-  ttl  = 300
-  records = [
-    "${aws_eip.master.public_ip}"
-  ]
+  name    = "console.${data.aws_route53_zone.public.name}"
+  type    = "A"
+  alias {
+    name    = "${aws_lb.console.dns_name}"
+    zone_id = "${aws_lb.console.zone_id}"
+    evaluate_target_health = "false"
+  }
 }
 resource "aws_route53_record" "apps" {
-  count = "${var.base_domain != "" ? 1 : 0}"
+  count   = "${var.base_domain != "" ? 1 : 0}"
   zone_id = "${data.aws_route53_zone.public.zone_id}"
-  name = "*.apps.${data.aws_route53_zone.public.name}"
-  type = "A"
-  ttl  = 300
+  name    = "*.apps.${data.aws_route53_zone.public.name}"
+  type    = "A"
+  ttl     = 300
   records = [
     "${aws_eip.master.public_ip}"
   ]
