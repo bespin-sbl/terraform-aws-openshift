350e6ffbb04abc29782343d37436930bbf5652c5
Changyu Seon <seonchg@gmail.com>
2018-07-03 15:36:52 +0900

    change directory names

--- modules/openshift-small/07-dns.tf
+++ modules/openshift-small/07-dns.tf
@@ -3,8 +3,9 @@
 
 //  Create the private DNS.
 resource "aws_route53_zone" "private" {
-  name    = "${var.cluster_name}.local"
-  vpc_id  = "${data.aws_vpc.openshift.id}"
+  name   = "${var.cluster_name}.local"
+  vpc_id = "${data.aws_vpc.openshift.id}"
+
   tags {
     Name    = "${var.cluster_name} Private DNS"
     Project = "openshift"
@@ -13,31 +14,47 @@ resource "aws_route53_zone" "private" {
 
 //  Routes for 'master', 'node1' and 'node2'.
 resource "aws_route53_record" "master-record" {
-    zone_id = "${aws_route53_zone.private.zone_id}"
-    name    = "master.${aws_route53_zone.private.name}"
-    type    = "A"
-    ttl     = 300
-    records = [
-        "${aws_instance.master.private_ip}"
-    ]
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name    = "master.${aws_route53_zone.private.name}"
+  type    = "A"
+  ttl     = 300
+
+  records = [
+    "${aws_instance.master.private_ip}",
+  ]
 }
+
 resource "aws_route53_record" "node1-record" {
-    zone_id = "${aws_route53_zone.private.zone_id}"
-    name    = "node1.${aws_route53_zone.private.name}"
-    type    = "A"
-    ttl     = 300
-    records = [
-        "${aws_instance.node1.private_ip}"
-    ]
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name    = "node1.${aws_route53_zone.private.name}"
+  type    = "A"
+  ttl     = 300
+
+  records = [
+    "${aws_instance.node1.private_ip}",
+  ]
 }
+
 resource "aws_route53_record" "node2-record" {
-    zone_id = "${aws_route53_zone.private.zone_id}"
-    name    = "node2.${aws_route53_zone.private.name}"
-    type    = "A"
-    ttl     = 300
-    records = [
-        "${aws_instance.node2.private_ip}"
-    ]
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name    = "node2.${aws_route53_zone.private.name}"
+  type    = "A"
+  ttl     = 300
+
+  records = [
+    "${aws_instance.node2.private_ip}",
+  ]
+}
+
+resource "aws_route53_record" "node3-record" {
+  zone_id = "${aws_route53_zone.private.zone_id}"
+  name    = "node3.${aws_route53_zone.private.name}"
+  type    = "A"
+  ttl     = 300
+
+  records = [
+    "${aws_instance.node3.private_ip}",
+  ]
 }
 
 //  Create the public DNS.
@@ -45,25 +62,30 @@ data "aws_route53_zone" "public" {
   count = "${var.base_domain != "" ? 1 : 0}"
   name  = "${var.base_domain}"
 }
+
 data "aws_acm_certificate" "public" {
-  count    = "${var.base_domain != "" ? 1 : 0}"
-  domain   = "${var.base_domain}"
+  count  = "${var.base_domain != "" ? 1 : 0}"
+  domain = "${var.base_domain}"
+
   statuses = [
-    "ISSUED"
+    "ISSUED",
   ]
+
   most_recent = true
 }
 
 //  Create the Console LB.
 resource "aws_lb" "console" {
-  count           = "${var.base_domain != "" ? 1 : 0}"
-  name            = "${var.cluster_name}-console-lb"
-  subnets         = ["${aws_subnet.public.*.id}"]
+  count   = "${var.base_domain != "" ? 1 : 0}"
+  name    = "${var.cluster_name}-console-lb"
+  subnets = ["${aws_subnet.public.*.id}"]
+
   security_groups = [
     "${aws_security_group.openshift-vpc.id}",
     "${aws_security_group.openshift-public-ingress.id}",
     "${aws_security_group.openshift-public-egress.id}",
   ]
+
   tags {
     Name = "${var.cluster_name} Console LB"
   }
@@ -75,16 +97,19 @@ resource "aws_lb_target_group" "console" {
   port     = "8443"
   protocol = "HTTPS"
   vpc_id   = "${data.aws_vpc.openshift.id}"
+
   tags {
     Name = "${var.cluster_name} Console LB TG"
   }
 }
+
 resource "aws_lb_target_group_attachment" "console" {
   count            = "${var.base_domain != "" ? 1 : 0}"
   target_group_arn = "${aws_lb_target_group.console.arn}"
   target_id        = "${aws_instance.master.id}"
   port             = "8443"
 }
+
 resource "aws_lb_listener" "console" {
   count             = "${var.base_domain != "" ? 1 : 0}"
   load_balancer_arn = "${aws_lb.console.arn}"
@@ -92,6 +117,7 @@ resource "aws_lb_listener" "console" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2015-05"
   certificate_arn   = "${data.aws_acm_certificate.public.arn}"
+
   default_action {
     target_group_arn = "${aws_lb_target_group.console.arn}"
     type             = "forward"
@@ -103,23 +129,26 @@ resource "aws_route53_record" "console" {
   zone_id = "${data.aws_route53_zone.public.zone_id}"
   name    = "console.${data.aws_route53_zone.public.name}"
   type    = "A"
+
   alias {
-    name    = "${aws_lb.console.dns_name}"
-    zone_id = "${aws_lb.console.zone_id}"
+    name                   = "${aws_lb.console.dns_name}"
+    zone_id                = "${aws_lb.console.zone_id}"
     evaluate_target_health = "false"
   }
 }
 
 //  Create the Apps LB.
 resource "aws_lb" "apps" {
-  count           = "${var.base_domain != "" ? 1 : 0}"
-  name            = "${var.cluster_name}-apps-lb"
-  subnets         = ["${aws_subnet.public.*.id}"]
+  count   = "${var.base_domain != "" ? 1 : 0}"
+  name    = "${var.cluster_name}-apps-lb"
+  subnets = ["${aws_subnet.public.*.id}"]
+
   security_groups = [
     "${aws_security_group.openshift-vpc.id}",
     "${aws_security_group.openshift-public-ingress.id}",
     "${aws_security_group.openshift-public-egress.id}",
   ]
+
   tags {
     Name = "${var.cluster_name} Apps LB"
   }
@@ -131,21 +160,25 @@ resource "aws_lb_target_group" "apps_http" {
   port     = "80"
   protocol = "HTTP"
   vpc_id   = "${data.aws_vpc.openshift.id}"
+
   tags {
     Name = "${var.cluster_name} Apps LB HTTP"
   }
 }
+
 resource "aws_lb_target_group_attachment" "apps_http" {
   count            = "${var.base_domain != "" ? 1 : 0}"
   target_group_arn = "${aws_lb_target_group.apps_http.arn}"
   target_id        = "${aws_instance.master.id}"
   port             = "80"
 }
+
 resource "aws_lb_listener" "apps_http" {
   count             = "${var.base_domain != "" ? 1 : 0}"
   load_balancer_arn = "${aws_lb.apps.arn}"
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
     target_group_arn = "${aws_lb_target_group.apps_http.arn}"
     type             = "forward"
@@ -158,16 +191,19 @@ resource "aws_lb_target_group" "apps_https" {
   port     = "443"
   protocol = "HTTPS"
   vpc_id   = "${data.aws_vpc.openshift.id}"
+
   tags {
     Name = "${var.cluster_name} Apps LB HTTPS"
   }
 }
+
 resource "aws_lb_target_group_attachment" "apps_https" {
   count            = "${var.base_domain != "" ? 1 : 0}"
   target_group_arn = "${aws_lb_target_group.apps_https.arn}"
   target_id        = "${aws_instance.master.id}"
   port             = "443"
 }
+
 resource "aws_lb_listener" "apps_https" {
   count             = "${var.base_domain != "" ? 1 : 0}"
   load_balancer_arn = "${aws_lb.apps.arn}"
@@ -175,6 +211,7 @@ resource "aws_lb_listener" "apps_https" {
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2015-05"
   certificate_arn   = "${data.aws_acm_certificate.public.arn}"
+
   default_action {
     target_group_arn = "${aws_lb_target_group.apps_https.arn}"
     type             = "forward"
@@ -186,9 +223,10 @@ resource "aws_route53_record" "apps" {
   zone_id = "${data.aws_route53_zone.public.zone_id}"
   name    = "*.apps.${data.aws_route53_zone.public.name}"
   type    = "A"
+
   alias {
-    name    = "${aws_lb.apps.dns_name}"
-    zone_id = "${aws_lb.apps.zone_id}"
+    name                   = "${aws_lb.apps.dns_name}"
+    zone_id                = "${aws_lb.apps.zone_id}"
     evaluate_target_health = "false"
   }
 }
