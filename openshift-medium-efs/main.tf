3ed61d5da48cdb85b6c283ef4827d32a7bc34b50
Changyu Seon <seonchg@gmail.com>
2018-07-09 11:42:39 +0900

    add medium-efs

--- openshift-medium-efs/main.tf
+++ openshift-medium-efs/main.tf
@@ -28,74 +28,74 @@ module "openshift" {
   base_domain     = "opspresso.com"
 }
 
-# //  Output some useful variables for quick SSH access etc.
-# output "admin-url" {
-#   value = "https://${module.openshift.console-public}:8443"
-# }
+//  Output some useful variables for quick SSH access etc.
+output "admin-url" {
+  value = "https://${module.openshift.console-public}:8443"
+}
 
-# output "master1-public_dns" {
-#   value = "${module.openshift.master1-public_dns}"
-# }
+output "master1-public_dns" {
+  value = "${module.openshift.master1-public_dns}"
+}
 
-# output "master1-public_ip" {
-#   value = "${module.openshift.master1-public_ip}"
-# }
+output "master1-public_ip" {
+  value = "${module.openshift.master1-public_ip}"
+}
 
-# output "master2-public_dns" {
-#   value = "${module.openshift.master2-public_dns}"
-# }
+output "master2-public_dns" {
+  value = "${module.openshift.master2-public_dns}"
+}
 
-# output "master2-public_ip" {
-#   value = "${module.openshift.master2-public_ip}"
-# }
+output "master2-public_ip" {
+  value = "${module.openshift.master2-public_ip}"
+}
 
-# output "master3-public_dns" {
-#   value = "${module.openshift.master3-public_dns}"
-# }
+output "master3-public_dns" {
+  value = "${module.openshift.master3-public_dns}"
+}
 
-# output "master3-public_ip" {
-#   value = "${module.openshift.master3-public_ip}"
-# }
+output "master3-public_ip" {
+  value = "${module.openshift.master3-public_ip}"
+}
 
-# output "node1-public_dns" {
-#   value = "${module.openshift.node1-public_dns}"
-# }
+output "node1-public_dns" {
+  value = "${module.openshift.node1-public_dns}"
+}
 
-# output "node1-public_ip" {
-#   value = "${module.openshift.node1-public_ip}"
-# }
+output "node1-public_ip" {
+  value = "${module.openshift.node1-public_ip}"
+}
 
-# output "node2-public_dns" {
-#   value = "${module.openshift.node2-public_dns}"
-# }
+output "node2-public_dns" {
+  value = "${module.openshift.node2-public_dns}"
+}
 
-# output "node2-public_ip" {
-#   value = "${module.openshift.node2-public_ip}"
-# }
+output "node2-public_ip" {
+  value = "${module.openshift.node2-public_ip}"
+}
 
-# output "infra1-public_dns" {
-#   value = "${module.openshift.infra1-public_dns}"
-# }
+output "infra1-public_dns" {
+  value = "${module.openshift.infra1-public_dns}"
+}
 
-# output "infra1-public_ip" {
-#   value = "${module.openshift.infra1-public_ip}"
-# }
+output "infra1-public_ip" {
+  value = "${module.openshift.infra1-public_ip}"
+}
 
-# output "infra2-public_dns" {
-#   value = "${module.openshift.infra2-public_dns}"
-# }
+output "infra2-public_dns" {
+  value = "${module.openshift.infra2-public_dns}"
+}
 
-# output "infra2-public_ip" {
-#   value = "${module.openshift.infra2-public_ip}"
-# }
+output "infra2-public_ip" {
+  value = "${module.openshift.infra2-public_ip}"
+}
 
-# output "bastion-public_dns" {
-#   value = "${module.openshift.bastion-public_dns}"
-# }
+output "bastion-public_dns" {
+  value = "${module.openshift.bastion-public_dns}"
+}
 
-# output "bastion-public_ip" {
-#   value = "${module.openshift.bastion-public_ip}"
-# }
+output "bastion-public_ip" {
+  value = "${module.openshift.bastion-public_ip}"
+}
 
 output "efs-id" {
   value = "${module.openshift.efs-id}"
